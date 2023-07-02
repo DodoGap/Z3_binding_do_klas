85a56947da27e0e43d4dab8433083b1fa3d08072
Dominik <gawronski.dom@gmail.com>
2023-07-02 22:36:19 +0200

    0.0.1

---
+++ MainListaAlbumow.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Z3_binding_do_klas
+{
+    public class MainListaAlbumow : INotifyPropertyChanged
+    {
+        private const string sciezkaIO = "./listaAlbumow.xml";
+        public ObservableCollection<Album> ListaAlbumow { get; set; } = new ObservableCollection<Album>();
+
+        private Album wybranyAlbum;
+        public Album WybranyAlbum
+        {
+            get { return wybranyAlbum; }
+            set
+            {
+                wybranyAlbum = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainListaAlbumow()
+        {
+            ImportujAlbumy();
+        }
+
+        public void DodajAlbum(Album album)
+        {
+            ListaAlbumow.Add(album);
+            ZapiszAlbumy();
+        }
+
+        public void EdytujAlbum(Album album)
+        {
+            ZapiszAlbumy();
+        }
+
+        public void UsunAlbum(Album album)
+        {
+            ListaAlbumow.Remove(album);
+            ZapiszAlbumy();
+        }
+
+        public void ImportujAlbumy()
+        {
+            if (File.Exists(sciezkaIO))
+            {
+                XmlSerializer serializator = new XmlSerializer(typeof(ObservableCollection<Album>));
+                using (FileStream strumienOdczytu = new FileStream(sciezkaIO, FileMode.Open))
+                {
+                    ObservableCollection<Album> albumy = (ObservableCollection<Album>)serializator.Deserialize(strumienOdczytu);
+                    ListaAlbumow = albumy;
+                }
+            }
+        }
+
+        public void ZapiszAlbumy()
+        {
+            XmlSerializer serializator = new XmlSerializer(typeof(ObservableCollection<Album>));
+            using (TextWriter strumienZapisu = new StreamWriter(sciezkaIO))
+            {
+                serializator.Serialize(strumienZapisu, ListaAlbumow);
+            }
+        }
+
+        public void EksportujAlbumy()
+        {
+            XmlSerializer serializator = new XmlSerializer(typeof(ObservableCollection<Album>));
+            using (TextWriter strumienZapisu = new StreamWriter(sciezkaIO))
+            {
+                serializator.Serialize(strumienZapisu, ListaAlbumow);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ OknoEdycjaAlbumu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Z3_binding_do_klas
+{
+    public partial class OknoEdycjaAlbumu : Window
+    {
+        public OknoEdycjaAlbumu(Album album)
+        {
+            InitializeComponent();
+            DataContext = album;
+
+        }
+
+        private void OK(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
+
